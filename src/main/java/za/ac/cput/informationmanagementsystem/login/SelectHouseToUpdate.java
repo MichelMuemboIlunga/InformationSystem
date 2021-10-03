881f8869d823d7b677e706922100ec9e12c8d4aa
michel muembo <michelmuemboilunga@gmail.com>
2021-10-03 18:53:19 +0200

    admin func done

--- src/main/java/za/ac/cput/informationmanagementsystem/login/SelectHouseToUpdate.java
+++ src/main/java/za/ac/cput/informationmanagementsystem/login/SelectHouseToUpdate.java
@@ -7,7 +7,8 @@ package za.ac.cput.informationmanagementsystem.login;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
+import java.awt.Container;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -19,20 +20,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
-
+import javax.swing.ButtonModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -44,21 +46,23 @@ import za.ac.cput.informationmanagementsystem.login.connection.DbConnection;
  *
  * @author MICHEL MUEMBO ILUNGA
  */
-public class AdminHouseAdd extends JFrame implements ActionListener{
-     
-   private JPanel panelNorth;
+public class SelectHouseToUpdate extends JFrame implements ActionListener {
+    
+    private JPanel panelNorth;
     private JPanel panelSouth;
     private JPanel panelEast;
-
+    
+    private JLabel lblSelectHouse;
     private JLabel lblHouseOwner;
-    private JLabel lblAddress;
+   
     private JLabel lblPrice;
     private JLabel lblHouseStatus;
-    
     private JLabel RecordMessage;
-
+    
+    private JComboBox SelectHouse;
+    private JButton GetAddress;
     private JTextField OwnerField;
-    private JTextField AddressField;
+  
     private JTextField PriceField;
     private JTextArea RecordMessageField;
     
@@ -73,7 +77,7 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
 
     private JFrame frame;
 
-    AdminHouseAdd(){
+    SelectHouseToUpdate(){
     
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -98,7 +102,7 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
         panelNorth.setPreferredSize(new Dimension(100,100));
 
         // set The title
-        JLabel title = new JLabel("House Management", JLabel.CENTER);
+        JLabel title = new JLabel("Update House ", JLabel.CENTER);
         title.setForeground(Color.WHITE);
         title.setFont(new Font("Serif", Font.BOLD, 50));
         title.setPreferredSize(new Dimension(500,90));
@@ -131,7 +135,36 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
         add(newPanelContent);
 
         
-
+        lblSelectHouse = new JLabel("Select house Address: ");
+        lblSelectHouse.setFont(new Font("Arial", Font.PLAIN, 20));
+        lblSelectHouse.setForeground(Color.WHITE);
+        
+        SelectHouse = new JComboBox();
+        SelectHouse.setPreferredSize(new Dimension(250,30));
+        SelectHouse.setFont(new Font("Arial", Font.PLAIN, 20));
+        
+        GetAddress = new JButton("Get Addresses");
+        GetAddress.setBackground(new Color(19, 22, 54));
+        GetAddress.setFont(new Font("Arial", Font.PLAIN, 15));
+        GetAddress.setForeground(Color.WHITE);
+        GetAddress.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        GetAddress.addActionListener(this);
+        
+        // positioning ID Field and label
+        
+        
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        newPanelContent.add(lblSelectHouse, constraints);
+        
+        constraints.gridx = 1;
+        newPanelContent.add(SelectHouse, constraints);
+        
+        constraints.gridx = 2;
+        constraints.gridy = 1;
+        newPanelContent.add(GetAddress, constraints);
+        
+                
         lblHouseOwner = new JLabel("House Owner: ");
         lblHouseOwner.setFont(new Font("Arial", Font.PLAIN, 20));
        
@@ -143,27 +176,17 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
     
         // positioning ID Field and label
         constraints.gridx = 0;
-        constraints.gridy = 1;
+        constraints.gridy = 2;
         newPanelContent.add(lblHouseOwner, constraints);
 
         constraints.gridx = 1;
         newPanelContent.add(OwnerField, constraints);
 
         
-        lblAddress = new JLabel("house Address: ");
-        lblAddress.setFont(new Font("Arial", Font.PLAIN, 20));
-        lblAddress.setForeground(Color.WHITE);
-        AddressField = new JTextField();
-        AddressField.setPreferredSize(new Dimension(250,30));
-        AddressField.setFont(new Font("Arial", Font.PLAIN, 20));
+      
     
-        // positioning First Name Field and label
-        constraints.gridx = 0;
-        constraints.gridy = 2;
-        newPanelContent.add(lblAddress, constraints);
-
-        constraints.gridx = 1;
-        newPanelContent.add(AddressField, constraints);
+    
+       
 
         // price
         lblPrice = new JLabel("House price: ");
@@ -175,7 +198,7 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
     
         // positioning last Name Field and label
         constraints.gridx = 0;
-        constraints.gridy = 3;
+        constraints.gridy = 4;
         newPanelContent.add(lblPrice, constraints);
 
         constraints.gridx = 1;
@@ -214,7 +237,7 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
         constraints.gridy = 9;
         newPanelContent.add(lblHouseStatus, constraints);
         
-        radBtnAvailable = new JRadioButton("Available",true);
+        radBtnAvailable = new JRadioButton("Available");
         radBtnRent = new JRadioButton("Rented");
         btnGroupHouse = new ButtonGroup();
         btnGroupHouse.add(radBtnAvailable);
@@ -230,7 +253,7 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
 
         // buttons
 
-        btnAdd = new JButton("Add House");
+        btnAdd = new JButton("Select House");
         btnAdd.setBackground(new Color(19, 22, 54));
         btnAdd.setForeground(Color.WHITE);
         btnAdd.setFont(btnAdd.getFont().deriveFont(20.0f));
@@ -238,7 +261,7 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
         btnAdd.addActionListener(this);
         
         
-        btnView = new JButton("View Houses");
+        btnView = new JButton("Update House");
         btnView.setBackground(new Color(19, 22, 54));
         btnView.setForeground(Color.WHITE);
         btnView.setFont(btnView.getFont().deriveFont(20.0f));
@@ -263,11 +286,14 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
         panelSouth.add(btnEXit);
         this.add(panelNorth,BorderLayout.NORTH);
         this.add(panelSouth, BorderLayout.SOUTH);
+        
+        
+        
 
 
     }
     
-    // alert box 
+      // alert box 
     public void ConfirmAlert(){
         
                 ImageIcon icon = new ImageIcon("src/images/lock64.png");
@@ -319,23 +345,76 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
                 }
     
     }
- 
-
-    // action perform section
-
+    
     public void actionPerformed(ActionEvent e) {
+        
+        if(e.getActionCommand().equals("Get Addresses")){
+            
+             try (Connection conn = DbConnection.DbConnection()) {
+                    
+                  
+                     java.sql.Statement statement = conn.createStatement();
+                     String Query = "SELECT house_address FROM house";
+                      
+                     
+                     ResultSet result = statement.executeQuery(Query);
 
-        if(e.getSource() == btnAdd){
-            // create conn
-             // create connection
-           
+                         
+                     while(result.next()){
+                             String venueName = result.getString("house_address");
+                             
+                             SelectHouse.addItem(venueName);
+                                             
+                     
+                     }
+
+                        
+                    } catch (SQLException ex) {
+                Logger.getLogger(SelectHouseToUpdate.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }else if(e.getActionCommand().equals("Select House")){
+             try (Connection conn = DbConnection.DbConnection()) {
+                    
+                     String addressVal = (String) SelectHouse.getSelectedItem();
+                     java.sql.Statement statement = conn.createStatement();
+                     String Query = "SELECT house_owner, house_price, house_description, house_status FROM house "
+                             + "WHERE house_address ='" + addressVal + "' ";
+                      
+                     
+                     ResultSet result = statement.executeQuery(Query);
+
+                         
+                     while(result.next()){
+                           
+                             String houseOwner = result.getString("house_owner");
+                             String housePrice = result.getString("house_price");
+                             String houseDescription = result.getString("house_description");
+                             String houseStatus = result.getString("house_status");
+                             
+                             OwnerField.setText(houseOwner);
+                             PriceField.setText(housePrice);
+                             RecordMessageField.setText(houseDescription);
+                             if("Available".equals(houseStatus)){
+                                 radBtnAvailable.setSelected(true);
+                             }else if("Rented".equals(houseStatus)){
+                                 radBtnRent.setSelected(true);
+                             }
+
+                     }
+
+                        
+                    } catch (SQLException ex) {
+                Logger.getLogger(SelectHouseToUpdate.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }else if(e.getActionCommand().equals("Update House")){
+            // update records
                 try (Connection conn = DbConnection.DbConnection()) {
                     
                     if(radBtnAvailable.isSelected()){
-                         
+                    String HouseAddres = (String) SelectHouse.getSelectedItem();
                     String HouseOwner = OwnerField.getText();
-                    String HouseAddress = AddressField.getText();
-                    String HousePrice =  PriceField.getText();
+                    int HousePrice =  Integer.parseInt(PriceField.getText());
                     String HouseMessage = RecordMessageField.getText();
                     String houseStatus = radBtnAvailable.getText();
                     
@@ -343,27 +422,28 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
                      // validations
                         
                     // set textfield red border if empty error (if login failed)
-                    if (OwnerField.getText().trim().isEmpty() || AddressField.getText().trim().isEmpty() || PriceField.getText().trim().isEmpty() || RecordMessageField.getText().trim().isEmpty()) {
+                    if (OwnerField.getText().trim().isEmpty() || PriceField.getText().trim().isEmpty() || RecordMessageField.getText().trim().isEmpty()) {
                          Border border = BorderFactory.createLineBorder(Color.RED, 2);
                          
                          JOptionPane.showMessageDialog(null, "Please Fill Up All The field","Error",JOptionPane.ERROR_MESSAGE);
                     }else{
-                        // insert data to table
+                        // update data to table
                     
                     java.sql.Statement statement = conn.createStatement();
                      PreparedStatement prep;
                   
                         String sql = 
-                                "INSERT INTO house(house_address, house_owner, house_price, house_description, house_status) "
-                               + "VALUES (?,?,?,?,?)";
+                                "UPDATE house SET house_owner =?, house_price =?, house_description =?, house_status=? "
+                                + "WHERE house_address =?";
                     
                               
                         prep = conn.prepareStatement(sql);
-                        prep.setString(1, HouseAddress);
-                        prep.setString(2, HouseOwner);
-                        prep.setString(3, HousePrice);
-                        prep.setString(4, HouseMessage);
-                        prep.setString(5, houseStatus);
+           
+                        prep.setString(1, HouseOwner);
+                        prep.setInt(2, HousePrice);
+                        prep.setString(3, HouseMessage);
+                        prep.setString(4, houseStatus);
+                        prep.setString(5, HouseAddres);
                         
                         prep.execute();
                         
@@ -374,37 +454,39 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
                     }
                   }else if(radBtnRent.isSelected()){
                             
-                      
-                            String HouseOwner = OwnerField.getText();
-                            String HouseAddress = AddressField.getText();
-                            String HousePrice =  PriceField.getText();
-                            String HouseMessage = RecordMessageField.getText();
-                            String houseStatus = radBtnRent.getText();
+                        String HouseAddres = (String) SelectHouse.getSelectedItem();
+                        String HouseOwner = OwnerField.getText();
+                        int HousePrice =  Integer.parseInt(PriceField.getText());
+                        String HouseMessage = RecordMessageField.getText();
+                        String houseStatus = radBtnRent.getText();
+                       
                     
+                      
                      // validations
                         
                     // set textfield red border if empty error (if login failed)
-                    if (OwnerField.getText().trim().isEmpty() || AddressField.getText().trim().isEmpty() || PriceField.getText().trim().isEmpty() || RecordMessageField.getText().trim().isEmpty()) {
+                    if (OwnerField.getText().trim().isEmpty() || PriceField.getText().trim().isEmpty() || RecordMessageField.getText().trim().isEmpty()) {
                          Border border = BorderFactory.createLineBorder(Color.RED, 2);
                          
                          JOptionPane.showMessageDialog(null, "Please Fill Up All The field","Error",JOptionPane.ERROR_MESSAGE);
                     }else{
-                        // insert data to table
+                        // update data to table
                     
                     java.sql.Statement statement = conn.createStatement();
                      PreparedStatement prep;
                   
                         String sql = 
-                                "INSERT INTO house(house_address, house_owner, house_price, house_description, house_status) "
-                               + "VALUES (?,?,?,?,?)";
+                                "UPDATE house SET house_owner =?, house_price =?, house_description =?, house_status=? "
+                                + "WHERE house_address =?";
                     
                               
                         prep = conn.prepareStatement(sql);
-                        prep.setString(1, HouseAddress);
-                        prep.setString(2, HouseOwner);
-                        prep.setString(3, HousePrice);
-                        prep.setString(4, HouseMessage);
-                        prep.setString(5, houseStatus);
+           
+                        prep.setString(1, HouseOwner);
+                        prep.setInt(2, HousePrice);
+                        prep.setString(3, HouseMessage);
+                        prep.setString(4, houseStatus);
+                        prep.setString(5, HouseAddres);
                         
                         prep.execute();
                         
@@ -415,32 +497,14 @@ public class AdminHouseAdd extends JFrame implements ActionListener{
                 }
 
                 } catch (SQLException sqlException) {
-                  JOptionPane.showMessageDialog(null, "something is wrong","Error",JOptionPane.ERROR_MESSAGE);
+                  JOptionPane.showMessageDialog(null,sqlException.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
 
                 }
            
-        }if(e.getSource() == btnView){
-             UpdateHouses update_house = new UpdateHouses();
-                    
-                    update_house.setTitle("INFROMATION MANAGEMENT SYSTEM");
-                    update_house.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-                    update_house.setSize(700, 700);
-                    update_house.setResizable(true);
-                    update_house.getContentPane().setBackground(new Color(19, 22, 54));
-                    update_house.setForeground(Color.WHITE);
-
-                    ImageIcon logo = new ImageIcon("src/logo.png");
-                    update_house.setIconImage(logo.getImage());
-          
-                    update_house.show();
-            
-        }if(e.getSource() == btnEXit){
+        }else if(e.getActionCommand().equals("Exit")){
             System.exit(0);
         }
-       
-          
-
-    }
-
+        
+    }      
+    
 }
-
--- src/main/java/za/ac/cput/informationmanagementsystem/login/UpdateHouses.java
+++ src/main/java/za/ac/cput/informationmanagementsystem/login/UpdateHouses.java
@@ -5,50 +5,185 @@
  */
 package za.ac.cput.informationmanagementsystem.login;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import za.ac.cput.informationmanagementsystem.login.connection.DbConnection;
 
 /**
  *
  * @author MICHEL MUEMBO ILUNGA
  */
-public class UpdateHouses extends JFrame {
-    // FORM TO ADD HOUSE
-    
+public class UpdateHouses extends JFrame implements ActionListener{
+   
      JLabel label = new JLabel();
-    JFrame frame = new JFrame();
+     JFrame frame = new JFrame();
 
-    UpdateHouses() {
+
+       UpdateHouses()  {
+        
+        showJtable();
+      
+       }
+       
+    public void showJtable() {
+        ArrayList columnNames = new ArrayList();
+        ArrayList data = new ArrayList();
+
+        String sql = "SELECT * FROM house";
+
+        Connection conn = null;
+         try {
+             conn = DbConnection.DbConnection();
+         } catch (SQLException ex) {
+             Logger.getLogger(UpdateHouses.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         
+        Statement stmt = null;
+         try {
+             stmt = conn.createStatement();
+         } catch (SQLException ex) {
+             Logger.getLogger(UpdateHouses.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         
+        ResultSet rs = null;
+         try {
+             rs = stmt.executeQuery(sql);
+         } catch (SQLException ex) {
+             Logger.getLogger(UpdateHouses.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         
+        ResultSetMetaData md = null;
+         try {
+             md = rs.getMetaData();
+         } catch (SQLException ex) {
+             Logger.getLogger(UpdateHouses.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         
+        int columns = 0;
+         try {
+             columns = md.getColumnCount();
+         } catch (SQLException ex) {
+             Logger.getLogger(UpdateHouses.class.getName()).log(Level.SEVERE, null, ex);
+         }
+        for (int i = 1; i <= columns; i++) {
+            try {
+                columnNames.add(md.getColumnName(i));
+            } catch (SQLException ex) {
+                Logger.getLogger(UpdateHouses.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+         try {
+             while (rs.next()) {
+                 ArrayList row = new ArrayList(columns);
+                 for (int i = 1; i <= columns; i++) {
+                     row.add(rs.getObject(i));
+                 }
+                 data.add(row);
+             }} catch (SQLException ex) {
+             Logger.getLogger(UpdateHouses.class.getName()).log(Level.SEVERE, null, ex);
+         }
+         
+         
+        Vector columnNamesVector = new Vector();
+        Vector dataVector = new Vector();
+        for (int i = 0; i < data.size(); i++) {
+          ArrayList subArray = (ArrayList) data.get(i);
+          Vector subVector = new Vector();
+          for (int j = 0; j < subArray.size(); j++) {
+            subVector.add(subArray.get(j));
+          }
+          dataVector.add(subVector);
+        }
+        for (int i = 0; i < columnNames.size(); i++)
+          columnNamesVector.add(columnNames.get(i));
+        JTable table = new JTable(dataVector, columnNamesVector) {
+           
+          public Class getColumnClass(int column) {
+            for (int row = 0; row < getRowCount(); row++) {
+              Object o = getValueAt(row, column);
+              if (o != null) {
+                return o.getClass();
+              }
+            }
+            return Object.class;
+          }
+        };
+        
+        table.setOpaque(true);
+        table.setFillsViewportHeight(true);
+        table.setBackground(new Color(19, 22, 54));
+        table.setForeground(Color.WHITE);
+        table.getTableHeader().setBackground(new Color(51,153,255));
+        table.getTableHeader().setForeground(Color.WHITE);
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        getContentPane().add(scrollPane);
+        
+        JPanel buttonPanel = new JPanel();
+        
+
+        JButton update = new JButton("Update record");
+        update.setPreferredSize(new Dimension(40, 40));
+        update.setFont(update.getFont().deriveFont(24.0f));
+        update.setForeground(Color.WHITE);
+        
+        update.addActionListener(this);
+        
+        update.setBackground(new Color(51,153,255));
+        update.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+        getContentPane().add(update,BorderLayout.SOUTH);
+      }
+        public void actionPerformed(ActionEvent e) {
+        
+         if(e.getActionCommand().equals("Update record")){
+              SelectHouseToUpdate s_update = new SelectHouseToUpdate();
+              s_update.setTitle("INFROMATION MANAGEMENT SYSTEM");
+              s_update.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+              s_update.setSize(700, 700);
+              s_update.setResizable(false);
+              s_update.getContentPane().setBackground(new Color(19, 22, 54));
+        
+              ImageIcon logo = new ImageIcon("src/logo.png");
+              s_update.setIconImage(logo.getImage());
+             
+              s_update.show();
+         }  
+         
+     }
+  }
+
     
-        DisplayPage();
-    }
-
-
-// create the interface
-    public void DisplayPage() {
-
-        // set the title of the page
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1000, 800);
-        this.setTitle("ANGRY FAST FOOD");
-        this.setSize(1000, 800);
-        this.setResizable(false);
-        this.getContentPane().setBackground(new Color(19, 22, 54));
-
-        ImageIcon food = new ImageIcon("src/Food.png");
-        this.setIconImage(food.getImage());
-        this.setVisible(true);
-
-        label.setText("Welcome To Agent dashboard");
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setVerticalAlignment(JLabel.CENTER);
-        label.setFont(new Font("Lato", Font.ITALIC, 15));
-        label.setForeground(Color.WHITE);
-
-        this.add(label);
-    }
+    
      
-}
+
+    
+     
+
+
